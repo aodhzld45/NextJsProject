e94cf1d94386f6699fdce83dba8442449310d370
hyunseok <prking94@naver.com>
2023-03-17 18:12:06 +0900

    2023_03_17 test commit

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,4 +1,5 @@
 import Animation from "../home/animation";
+import Link from "next/link";
 
 export default function Hero() {
     return (
@@ -14,8 +15,9 @@ export default function Hero() {
             장식하는 있는 이상, 뿐이다. 모래뿐일 끓는 것이 때까지 수 이 스며들어 되려니와, 이것이다. 유소년에게서 원질이 광야에서 용감하고 전인 청춘의 방지하는 황금시대다. 구하지 황금시대를 품었기 커다란 내려온 따뜻한 대중을 아름답고 칼이다. 사랑의 갑 남는 하는 밝은 속잎나고, 말이다. 하는 곳으로 꽃 낙원을 용감하고 것은 기쁘며, 방지하는 미묘한 있다. 이 청춘의 싹이 대중을 청춘의 피가 귀는 속에서 생생하며, 것이다. 기관과 천지는 그들의 그들의 같은 품으며, 장식하는 찾아 아름다우냐? 품에 그와 그들은 능히 구하기 피에 황금시대다.
               </p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                프로젝트 보러가기</button>
+              <a className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                프로젝트 보러가기
+            </a>
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,19 @@
+import Head from "next/head"
 import Layout from "../components/layout";
-
-
 
 export default function Projects() {
 
     return (
         <Layout>
+
+            <Head>
+                <title>현석이의 포트폴리오</title>
+                <meta name="description" content="오늘부터 다시 시작합니다" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
            <h1>프로젝트들</h1> 
+
         </Layout>
     )
     
