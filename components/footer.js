4b7f4193c0e745520f6a1c9d92c59559e4d32f50
hyunseok <prking94@naver.com>
2023-03-18 15:54:40 +0900

    2023-03-18 Link 설정

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
 
     return (
@@ -6,9 +8,13 @@ export default function Footer() {
           
             <div className="bg-gray-100">
                 <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-                <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                    <span className="ml-3 text-xl">현석이의 포트폴리오</span>
-                </a>
+
+                <Link href="/" legacyBehavior>
+                    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                        <span className="ml-3 text-xl">현석이의 포트폴리오</span>
+                    </a>
+                </Link>
+
                 <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
                     <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
                 </p>
